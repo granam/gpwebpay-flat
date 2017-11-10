05c133f69c61d432326b73efff58b264e0e10db0
Jaroslav Týc <mail@jaroslavtyc.com>
2017-11-10 15:58:19 +0100

    Day filter of transactions tested

--- Granam/Tests/GpWebPay/Flat/FlatReportParserTest.php
+++ Granam/Tests/GpWebPay/Flat/FlatReportParserTest.php
@@ -21,8 +21,7 @@ class FlatReportParserTest extends TestWithMockery
     {
         $flatReportParser = new FlatReportParser();
         $flatContentFromCzechFile = $flatReportParser->createFlatContentFromCzechFile(
-            __DIR__ . '/../../../Documentations/cs/Vzor FLAT.txt',
-            //             __DIR__ . '/data/VDAT-000819-123450001-123450001-20171110.TXT',
+            __DIR__ . '/data/VDAT-000819-123450001-123450001-20171110.TXT',
             new CzechECommerceTransactionHeaderMapper()
         );
         self::assertInstanceOf(FlatContent::class, $flatContentFromCzechFile);
@@ -34,6 +33,27 @@ class FlatReportParserTest extends TestWithMockery
         self::assertSame(241585.0, $eCommerceTransactions->getPaidAmountInMerchantCurrencySummary());
         self::assertSame(-1219.88, $eCommerceTransactions->getFeesInMerchantCurrencySummary());
         self::assertSame(240365.12, $eCommerceTransactions->getPaidAmountWithoutFeesSummary());
+
+        $silvesterTransactions = $flatContentFromCzechFile->getECommerceTransactions(new \DateTime('2017-12-31'));
+        self::assertNull($silvesterTransactions);
+
+        $mainDayTransactions = $flatContentFromCzechFile->getECommerceTransactions(new \DateTime('2017-11-09'));
+        self::assertNotNull($mainDayTransactions);
+        self::assertSame(266 - 5, $mainDayTransactions->count());
+        self::assertCount(266 - 5, $mainDayTransactions);
+        self::assertSame($mainDayTransactions->getSummary(), $mainDayTransactions->getPaidAmountInMerchantCurrencySummary());
+        self::assertSame(241585.0 - 5935.0, $mainDayTransactions->getPaidAmountInMerchantCurrencySummary());
+        self::assertSame(-1219.88 + 19.68, $mainDayTransactions->getFeesInMerchantCurrencySummary());
+        self::assertSame(240365.12 - 5915.32, $mainDayTransactions->getPaidAmountWithoutFeesSummary());
+
+        $beforeMainDayTransactions = $flatContentFromCzechFile->getECommerceTransactions(new \DateTime('2017-11-08'));
+        self::assertNotNull($beforeMainDayTransactions);
+        self::assertSame(5, $beforeMainDayTransactions->count());
+        self::assertCount(5, $beforeMainDayTransactions);
+        self::assertSame($beforeMainDayTransactions->getSummary(), $beforeMainDayTransactions->getPaidAmountInMerchantCurrencySummary());
+        self::assertSame(5935.0, $beforeMainDayTransactions->getPaidAmountInMerchantCurrencySummary());
+        self::assertSame(-19.68, $beforeMainDayTransactions->getFeesInMerchantCurrencySummary());
+        self::assertSame(5915.32, $beforeMainDayTransactions->getPaidAmountWithoutFeesSummary());
     }
 
     /**
